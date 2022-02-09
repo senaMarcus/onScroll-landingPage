a1c68529e63b63caa8796e0a0d663ef41ee6a7a7
Marcus Sena <maarvins.19@gmail.com>
2022-02-09 19:19:52 -0300

    add: showing the first pict in my page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-
 import './App.css';
+
+import Hero from './components/Hero'
+
+import travel_01 from "./assets/travel-01.jpg"
+import travel_02 from "./assets/travel-02.jpg"
+import travel_03 from "./assets/travel-03.jpg"
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello world!</h1>
+      <Hero imageSrc={travel_01}/>
     </div>
   );
 }
