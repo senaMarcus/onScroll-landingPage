664a607513420cf35f116478c4ba7da0343c8529
Marcus Sena <maarvins.19@gmail.com>
2022-02-09 20:02:35 -0300

    add: apply the SLIDER component on the project

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 
 import Hero from './components/Hero'
+import Slider from './components/Slider'
 
 import travel_01 from "./assets/travel-01.jpg"
 import travel_02 from "./assets/travel-02.jpg"
@@ -10,6 +11,17 @@ function App() {
   return (
     <div className="App">
       <Hero imageSrc={travel_01}/>
+      <Slider 
+      imageSrc={travel_02} 
+      title={"Seja um EXPLORADOR!"} 
+      subtitle={"Nossa plataforma oferece a você uma enorme variedade de lugares incríveis para conhecer! "}
+      />
+      <Slider 
+      imageSrc={travel_03} 
+      title={"Memórias para sua linha do tempo."} 
+      subtitle={"Sua viagem dos sonhos à apenas alguns cliques... "}
+      flipped={true}
+      />
     </div>
   );
 }
--- src/components/Slider/index.js
+++ src/components/Slider/index.js
@@ -1,4 +1,5 @@
 import React from "react"
+import './styles.css'
 
 const Slider = ({imageSrc, title, subtitle, flipped }) => { //flipped dira se a imagem estara a esquerda ou a direita
     
@@ -13,14 +14,14 @@ const Slider = ({imageSrc, title, subtitle, flipped }) => { //flipped dira se a 
                     </div>  
                 </>
                 )
-            } else{
+            } else {
                 return (
                 <>
-                        <img src={imageSrc} alt="Travel" className="slider__image" />
                         <div className="slider__content">
                             <h1 className="slider__title">{title}</h1>
                             <p>{subtitle}</p>
                         </div>  
+                        <img src={imageSrc} alt="Travel" className="slider__image" />
                 </>
                 )
             }
