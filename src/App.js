f4d7d671c81fe9e96592326477280bc4fed096f1
Marcus Sena <maarvins.19@gmail.com>
2022-02-09 22:06:28 -0300

    fix: edit h1 from hero component attributes

--- src/App.js
+++ src/App.js
@@ -10,14 +10,14 @@ import travel_03 from "./assets/travel-03.jpg"
 function App() {
   return (
     <div className="App">
-      <Hero imageSrc={travel_01}/>
+      <Hero imageSrc={travel_03}/>
       <Slider 
       imageSrc={travel_02} 
       title={"Seja um EXPLORADOR!"} 
       subtitle={"Nossa plataforma oferece a você uma enorme variedade de lugares incríveis para conhecer! "}
       />
       <Slider 
-      imageSrc={travel_03} 
+      imageSrc={travel_01} 
       title={"Memórias para sua linha do tempo."} 
       subtitle={"Sua viagem dos sonhos à apenas alguns cliques... "}
       flipped={true}
