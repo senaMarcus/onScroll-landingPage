bb794269ef1341adef1086bb11b66da5541a16c9
Marcus Sena <maarvins.19@gmail.com>
2022-02-09 23:43:38 -0300

    add: make responsive

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,28 +1,44 @@
-import React from "react"
-
-import './styles.css'
-
-import { FiMenu, FiX } from 'react-icons/fi'
-
-// const navbarLinks = [{url: "/home", title: "Home" }]
+import React, { useState } from "react";
+import "./styles.css";
+import { FiMenu, FiX } from "react-icons/fi";
 
 const Navbar = ({ navbarLinks }) => {
-    return (
-    <nav className="navbar">
-        <span className="navbar__logo">viajAir</span>
-        <ul className="navbar__list">
-            {navbarLinks.map((item) => {
-                return ( //o react sempre vai esperar uma CHAVE UNICA, nesse caso utilizaremos o titulo 
-                    <li className="navbar__item" key={item.title}> 
-                        <a className="navbar__link" href={item.url}>
-                            {item.title}
-                        </a>
-                    </li>
-                )
-            })}
-        </ul>
-    </nav>
-    )
-}
+  // Determines if the "menu icon" was clicked or not. Note that this icon is only visible when the window width is small.
+  const [menuClicked, setMenuClicked] = useState(false);
 
-export default Navbar
+  const toggleMenuClick = () => {
+    setMenuClicked(!menuClicked);
+  };
+
+  return (
+    <nav className="navbar">
+      <span className="navbar__logo">viajAir</span>
+      {menuClicked ? (
+        <FiX size={25} className={"navbar__menu"} onClick={toggleMenuClick} />
+      ) : (
+        <FiMenu
+          size={25}
+          className={"navbar__menu"}
+          onClick={toggleMenuClick}
+        />
+      )}
+      <ul
+        className={
+          menuClicked ? "navbar__list navbar__list--active" : "navbar__list"
+        }
+      >
+        {navbarLinks.map((item, index) => {
+          return (
+            <li className="navbar__item" key={index}>
+              <a className="navbar__link" href={item.url}>
+                {item.title}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
